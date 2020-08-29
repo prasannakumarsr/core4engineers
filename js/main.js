cd7427e3a17d251e67f7e181ecfbf5814144942f
Prasanna Kumar <prasannakumar027@gmail.com>
2020-08-29 23:14:58 +0530

    navigating pages

--- js/main.js
+++ js/main.js
@@ -1,55 +1,61 @@
-jQuery(document).ready(function($){
-	//if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
-	var MqL = 1170;
-	//move nav element position according to window width
-	moveNavigation();
-	$(window).on('resize', function(){
-		(!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
-	});
+jQuery(document).ready(function ($) {
+    //if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
+    var MqL = 1170;
+    //move nav element position according to window width
+    moveNavigation();
+    $(window).on('resize', function () {
+        (!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300): window.requestAnimationFrame(moveNavigation);
+    });
 
-	//mobile - open lateral menu clicking on the menu icon
-	$('.cd-nav-trigger').on('click', function(event){
-		event.preventDefault();
-		if( $('.cd-main-content').hasClass('nav-is-visible') ) {
-			closeNav();
-			$('.cd-overlay').removeClass('is-visible');
-		} else {
-			$(this).addClass('nav-is-visible');
-			$('.cd-main-header').addClass('nav-is-visible');
-			$('.cd-main-content').addClass('nav-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-				$('body').addClass('overflow-hidden');
-			});
-			toggleSearch('close');
-			$('.cd-overlay').addClass('is-visible');
-		}
-	});
+    //mobile - open lateral menu clicking on the menu icon
+    $('.cd-nav-trigger').on('click', function (event) {
+        event.preventDefault();
+        if ($('.cd-main-content').hasClass('nav-is-visible')) {
+            closeNav();
+            $('.cd-overlay').removeClass('is-visible');
+        } else {
+            $(this).addClass('nav-is-visible');
+            $('.cd-main-header').addClass('nav-is-visible');
+            $('.cd-main-content').addClass('nav-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+                $('body').addClass('overflow-hidden');
+            });
+            toggleSearch('close');
+            $('.cd-overlay').addClass('is-visible');
+        }
+    });
 
-	//open search form
-	$('.cd-search-trigger').on('click', function(event){
-		event.preventDefault();
-		toggleSearch();
-		closeNav();
-	});
+    //open search form
+    $('.cd-search-trigger').on('click', function (event) {
+        event.preventDefault();
+        toggleSearch();
+        closeNav();
+    });
 
-	
-	$(window).scroll(function () {
-			if ($(this).scrollTop() > 50) {
-				$('#back-to-top').fadeIn();
-			} else {
-				$('#back-to-top').fadeOut();
-			}
-		});
-		// scroll body to 0px on click
-		$('#back-to-top').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 400);
-			return false;
-		});
-	
+    if (location.hash) {
+        jQuery('html, body').animate({
+            scrollTop: jQuery(location.hash).offset().top
+        }, 400);
+    }
 
 
-	$('.customer-logos').slick({
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+
+
+
+    $('.customer-logos').slick({
         slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
@@ -70,63 +76,63 @@ jQuery(document).ready(function($){
         }]
     });
 
-	//submenu items - go back link
-	$('.go-back').on('click', function(){
-		$(this).parent('ul').addClass('is-hidden').parent('.has-children').parent('ul').removeClass('moves-out');
-	});
+    //submenu items - go back link
+    $('.go-back').on('click', function () {
+        $(this).parent('ul').addClass('is-hidden').parent('.has-children').parent('ul').removeClass('moves-out');
+    });
 
-	function closeNav() {
-		$('.cd-nav-trigger').removeClass('nav-is-visible');
-		$('.cd-main-header').removeClass('nav-is-visible');
-		$('.cd-primary-nav').removeClass('nav-is-visible');
-		$('.has-children ul').addClass('is-hidden');
-		$('.has-children a').removeClass('selected');
-		$('.moves-out').removeClass('moves-out');
-		$('.cd-main-content').removeClass('nav-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-			$('body').removeClass('overflow-hidden');
-		});
-	}
+    function closeNav() {
+        $('.cd-nav-trigger').removeClass('nav-is-visible');
+        $('.cd-main-header').removeClass('nav-is-visible');
+        $('.cd-primary-nav').removeClass('nav-is-visible');
+        $('.has-children ul').addClass('is-hidden');
+        $('.has-children a').removeClass('selected');
+        $('.moves-out').removeClass('moves-out');
+        $('.cd-main-content').removeClass('nav-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+            $('body').removeClass('overflow-hidden');
+        });
+    }
 
-	function toggleSearch(type) {
-		if(type=="close") {
-			//close serach 
-			$('.cd-search').removeClass('is-visible');
-			$('.cd-search-trigger').removeClass('search-is-visible');
-			$('.cd-overlay').removeClass('search-is-visible');
-		} else {
-			//toggle search visibility
-			$('.cd-search').toggleClass('is-visible');
-			$('.cd-search-trigger').toggleClass('search-is-visible');
-			$('.cd-overlay').toggleClass('search-is-visible');
-			if($(window).width() > MqL && $('.cd-search').hasClass('is-visible')) $('.cd-search').find('input[type="search"]').focus();
-			($('.cd-search').hasClass('is-visible')) ? $('.cd-overlay').addClass('is-visible') : $('.cd-overlay').removeClass('is-visible') ;
-		}
-	}
+    function toggleSearch(type) {
+        if (type == "close") {
+            //close serach 
+            $('.cd-search').removeClass('is-visible');
+            $('.cd-search-trigger').removeClass('search-is-visible');
+            $('.cd-overlay').removeClass('search-is-visible');
+        } else {
+            //toggle search visibility
+            $('.cd-search').toggleClass('is-visible');
+            $('.cd-search-trigger').toggleClass('search-is-visible');
+            $('.cd-overlay').toggleClass('search-is-visible');
+            if ($(window).width() > MqL && $('.cd-search').hasClass('is-visible')) $('.cd-search').find('input[type="search"]').focus();
+            ($('.cd-search').hasClass('is-visible')) ? $('.cd-overlay').addClass('is-visible'): $('.cd-overlay').removeClass('is-visible');
+        }
+    }
 
-	function checkWindowWidth() {
-		//check window width (scrollbar included)
-		var e = window, 
+    function checkWindowWidth() {
+        //check window width (scrollbar included)
+        var e = window,
             a = 'inner';
-        if (!('innerWidth' in window )) {
+        if (!('innerWidth' in window)) {
             a = 'client';
             e = document.documentElement || document.body;
         }
-        if ( e[ a+'Width' ] >= MqL ) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+        if (e[a + 'Width'] >= MqL) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	function moveNavigation(){
-		var navigation = $('.cd-nav');
-  		var desktop = checkWindowWidth();
-        if ( desktop ) {
-			navigation.detach();
-			navigation.insertBefore('.cd-header-buttons');
-		} else {
-			navigation.detach();
-			navigation.insertAfter('.cd-main-content');
-		}
-	}
+    function moveNavigation() {
+        var navigation = $('.cd-nav');
+        var desktop = checkWindowWidth();
+        if (desktop) {
+            navigation.detach();
+            navigation.insertBefore('.cd-header-buttons');
+        } else {
+            navigation.detach();
+            navigation.insertAfter('.cd-main-content');
+        }
+    }
 });
